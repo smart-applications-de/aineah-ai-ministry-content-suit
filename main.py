c7e1a837a21022287d77c77078f10e501dde8067
smart-applications-de <aineah.smart.application@gmail.com>
2025-08-15 23:19:55 +0200

    added sqlite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# app.py
-# Final, unified, and production-ready application code for the AI Ministry & Content Suite.
-
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 import os
